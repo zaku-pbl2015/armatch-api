c4898ffa720d111dcdf2aa7d5a240cb37b8d6b57
咲山拓哉 <zaku@zaku.local>
2015-11-25 11:39:10 +0900

    id指定student取得API作成

--- app/api/armatch.rb
+++ app/api/armatch.rb
@@ -1,12 +1,20 @@
 module Armatch
   class API < Grape::API
     format :json
+    #prefix :apiでアドレスがlocalhost:3000/api/...となる
     prefix :api
     resource :students do
-
       desc "Return all students information"
       get '/', jbuilder:'students' do
         @students = Student.all
+      end
+
+      desc "Return one student"
+      params do
+        requires :id, type: Integer, desc: "Person id"
+      end
+      get ':id' do
+        @students = Student.find(params[:id])
       end
     end
   end

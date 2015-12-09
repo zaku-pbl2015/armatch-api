7926f163f06bfd09c61947838fa2621a7d24dc6e
te2ka <yuki@Yuki-no-MacBook-Air.local>
2015-12-09 17:46:09 +0900

    #8; 企業情報を登録できるようにした

--- app/api/armatch/corporations.rb
+++ app/api/armatch/corporations.rb
@@ -15,15 +15,23 @@ module Armatch
         @corporations = Corporation.find(params[:id])
       end
 
-=begin
-#企業の情報更新API途中
-      desc "update Corporation"
+      desc "create corporation"
       params do
-        requires :id, type Integer, desc: "Corporation id"
+        requires :name    , type: String, desc: "Corporation name"
+        requires :outline , type: String, desc: "Corporation outline"
+        requires :email   , type: String, desc: "Email"
+        requires :password, type: String, desc: "Password"
       end
-      post ':id' do
+      post do
+        require 'digest/sha1'
+
+        Corporation.create!({
+          name: params[:name],
+          outline: params[:outline],
+          email: params[:email],
+          password: Digest::SHA1.hexdigest(params[:password])
+        })
       end
-=end
     end
   end
 end
